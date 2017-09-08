4173c417205a299d5eed4570e9c3f51eb75c5c2b
Sai Raghavendra Sankrantipati <sai.sankrantipati@colorado.edu>
2017-09-08 15:26:54 -0600

    Create sys_script.sh

---
+++ utils/sys_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+os=$(lsb_release -a)
+kv=$(uname -a)
+kb=$(cat /proc/version)
+sys_arch=$(uname -m)
+
+cat <<EOM >sysscript.txt
+
+
+OS Type and Distribution:
+$os
+
+Kernel Version:
+$kv
+
+Kernel Build:
+$kb
+
+System Architecture:
+$sys_arch
+
+EOM
