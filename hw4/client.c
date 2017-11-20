2c40fd04bff10a8a6fe70ffee93cf5a0396bd7fc
Sai Raghavendra Sankrantipati <sai.sankrantipati@colorado.edu>
2017-11-20 00:22:36 -0700

    Update client.c

--- hw4/client.c
+++ hw4/client.c
@@ -8,9 +8,25 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
+/**********************************************************************
+*@Filename:client.c
+*
+*@Description: This is client part of socket connection. 
+*@Author:Sai Raghavendra Sankrantipati
+*@Date:11/19/2017
+*@compiler:arm-linux-gnueabihf-gcc
+*@Usage : run gcc client.c -o client
+	$ ./client <ip.address> <port_no>
+	in my case
+	$ ./client 192.168.7.2 33333
+	usable only in internet domain. Doesn't work for Unix domain
+*@Reference: http://www.cs.rpi.edu/~moorthy/Courses/os98/Pgms/socket.html
+ **********************************************************************/
+
 #define LED_OFF 0
 #define LED_ON 1
 
+/*Enum for socket communication*/
 typedef enum {
 	write_period,
 	write_duty_cycle,
@@ -22,17 +38,23 @@ typedef enum {
 	exit_conn
 }state_variable_t;
 
+/*This struct is transferred in socket communication*/
 struct user_struct{
-	state_variable_t state_variable;
-	int value;
+	state_variable_t state_variable; 	//Enum of type of messsage
+	int value;				//Value of the message
 };
 
+/*Prints error message in stderr*/
 void error(char *msg)
 {
     perror(msg);
     exit(0);
 }
 
+/*Takes 2 arguments along with file name
+ *ip address of server at argv[1] 
+ *port number at argv[2]
+ */
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
@@ -41,28 +63,35 @@ int main(int argc, char *argv[])
     struct hostent *server;
 
     char buffer[256];
+
+    /*Check for 3 arguments in command line*/
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
-
+   
+    /*Ascii value of input port number is converted to integer*/	
     portno = atoi(argv[2]);
+    /*Socket system call creates a new socket, returns file pointer*/	
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
+    /*Takes name as an argument retuns pointer to hostnet*/	
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
+    /*bzero sets all values in a buffer to zero*/
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
+    /*bcopy() is a predated version of POSIX, Use memcpy insteadd*/
     bcopy((char *)server->h_addr,
          (char *)&serv_addr.sin_addr.s_addr,
          server->h_length);
     serv_addr.sin_port = htons(portno);
 
-
+    /*It establishes connection with server*/
     if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
     printf("Client: Connection established with %s\n",argv[1]);
@@ -70,39 +99,33 @@ int main(int argc, char *argv[])
     int count = 0;
     struct user_struct us;
     while(1){
-
-
-    	printf("count = %d\n",count );
+	/*count increases by 1 for every 1 second. All if and elses are self explanatory.
+	 *Once every 1 second, each of enum state_varaiable_t is sent as message*/
     	if( count == write_period){
-    		printf("count = %d\n",count );
     	    struct user_struct us = {
     	    		htons(write_period),
     	    		htons(5),
     	    };
     	}
     	else if( count == write_duty_cycle){
-    		printf("count = %d\n",count );
     	    struct user_struct us = {
     	    		htons(write_duty_cycle),
     	    		htons(99),
     	    };
     	}
     	else if( count == write_state){
-    		printf("count = %d\n",count );
     	    struct user_struct us = {
     	    		htons(write_state),
     	    		htons(LED_OFF),
     	    };
     	}
     	else if( count == read_period){
-    		printf("count = %d\n",count );
     	    struct user_struct us = {
     	    		htons(read_period),
     	    		htons(0),
     	    };
     	}
     	else if( count == read_duty_cycle){
-    		printf("count = %d\n",count );
     	    struct user_struct us = {
     	    		htons(read_duty_cycle),
     	    		htons(0),
@@ -127,17 +150,19 @@ int main(int argc, char *argv[])
     	    };
     	}
 
+	/*write struct to socket file descriptor*/
     	n = write(sockfd, (void *) &us, sizeof(us));
     	if (n < 0)
     		error("ERROR writing to socket");
     	bzero(buffer,256);
+	/*socket gives acknowledgement messagee*/
     	n = read(sockfd,buffer,255);
     	if (n < 0)
     		error("ERROR reading from socket");
     	printf("Client: %s\n",buffer);
     	sleep(1);
     	count++;
-
+	/*Client closes after 7 loops*/    
     	if(count == exit_conn)
     		return 0;
     }
